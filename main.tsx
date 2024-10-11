16633cc3da32f14e8c2db8a497aa85ef1c712dd5
S01an <s01an.xc@gmail.com>
2024-10-11 22:56:28 +0800

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,15 @@
+import blog from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+  author: "s01an",
+  title: "s01an'Blog",
+  description: "人望山 鱼窥荷",
+  avatar: "https://smms.app/image/e6vT84nXPDmgkBL",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:bot@deno.com" },
+    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "Twitter", url: "https://twitter.com/denobot" },
+  ],
+  lang: "zh",
+});
